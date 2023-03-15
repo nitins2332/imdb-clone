2ddefbc59884708dae7143d623fc4ccafb7aac09
Nitins <nitins2332@gmail.com>
2023-03-15 21:15:41 +0530

    create header menu with material UI

---
+++ src/Components/Common/Header.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  InputBase,
+  styled,
+} from "@mui/material";
+import { logoURL } from "../../Constants/Constant";
+
+import { Menu, BookmarkAdd, ArrowDropDown } from "@mui/icons-material";
+
+// Component
+import HeaderMenu from "./HeaderMenu";
+
+const StyledToolbar = styled(Toolbar)`
+  background: #121212;
+  min-height: 56px !important;
+  padding: 0 115px !important;
+  justify-content: space-between;
+  & > * {
+    padding: 0 15px;
+  }
+  & > div {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    & > p {
+      font-size: 14px;
+      font-weight: 600;
+    }
+  }
+  & > p {
+    font-size: 14px;
+    font-weight: 600;
+  }
+`;
+
+const InputSearchField = styled(InputBase)`
+  background: #fff;
+  height: 30px;
+  width: 55%;
+  border-radius: 5px;
+  padding: 10px;
+  font-size: 14px;
+`;
+
+const Logo = styled("img")({
+  width: 64,
+});
+
+const Header = () => {
+  const [open, setOpen] = useState(null);
+
+  const handleClick = (event) => {
+    setOpen(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setOpen(null);
+  };
+
+  return (
+    <>
+      <AppBar>
+        <StyledToolbar>
+          <Logo src={logoURL} alt="logo" />
+          <Box onClick={handleClick}>
+            <Menu />
+            <Typography>Menu</Typography>
+          </Box>
+          <HeaderMenu open={open} handleClose={handleClose} />
+          <InputSearchField />
+          <Typography>
+            IMDb<Box component="span">Pro</Box>
+          </Typography>
+          <Box>
+            <BookmarkAdd />
+            <Typography>Watchlist</Typography>
+          </Box>
+          <Typography>Sign In</Typography>
+          <Box>
+            <Typography>EN</Typography>
+            <ArrowDropDown />
+          </Box>
+        </StyledToolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Header;
